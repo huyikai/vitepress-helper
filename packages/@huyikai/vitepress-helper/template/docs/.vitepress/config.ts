072c8cd8a132a4340635ece459fe3a68652dd103
huyikai <hyk_tiger@foxmail.com>
2023-12-22 14:30:16 +0800

    refactor: 💡 modify template

--- packages/@huyikai/vitepress-helper/template/docs/.vitepress/config.ts
+++ packages/@huyikai/vitepress-helper/template/docs/.vitepress/config.ts
@@ -10,16 +10,12 @@ const vitepressConfig = {
   title: 'vitepres-helper',
   description: 'vitepres-helper.',
   head: [
-    ['link', { rel: 'icon', href: '/vitepress-helper/favicon.ico' }] //浏览器标签icon
+    ['link', { rel: 'icon', href: '/favicon.ico' }] //浏览器标签icon
   ],
   themeConfig: {
     siteTitle: 'VitePress-Helper', //导航栏左侧名称
     logo: '/static/nav-logo.svg', //导航栏左侧头像
-    lastUpdated: true, //最后更新时间
     outlineTitle: 'Catalog', //右侧 侧边栏标题
-    search: {
-      provider: 'local' // 离线搜索
-    },
     // 导航栏
     nav: [
       {
@@ -38,11 +34,6 @@ const vitepressConfig = {
     socialLinks: [
       { icon: 'github', link: 'https://github.com/huyikai/vitepress-helper' }
     ],
-    // 网站页脚
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022'
-    },
     // 文档页脚-上下页显示文字
     docFooter: {
       prev: 'Pervious',
