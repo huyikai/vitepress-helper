c0f3ffd94f4fd5956cc10f4d69a668355e723284
huyikai <hyk_tiger@foxmail.com>
2024-03-08 17:02:04 +0800

    style: 💄 import icon css

--- packages/@huyikai/vitepress-helper/theme/index.ts
+++ packages/@huyikai/vitepress-helper/theme/index.ts
@@ -1,6 +1,7 @@
 import 'vitepress/dist/client/theme-default/styles/fonts.css';
 import 'vitepress/dist/client/theme-default/styles/vars.css';
 import 'vitepress/dist/client/theme-default/styles/base.css';
+import 'vitepress/dist/client/theme-default/styles/icons.css'
 import 'vitepress/dist/client/theme-default/styles/utils.css';
 import 'vitepress/dist/client/theme-default/styles/components/custom-block.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-code.css';
